ca643acaa429eb038f73f360a7e823f4223d86ba
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-03-25 15:45:06 +0530

    fix: removing the bug code of code snippets in lua for neovim

--- init.lua
+++ init.lua
@@ -885,19 +885,6 @@ require('lazy').setup({
     },
   },
 })
-local luasnip = require 'luasnip'
-
-vim.keymap.set({ 'i', 's' }, '<Tab>', function()
-  if luasnip.expand_or_jumpable() then
-    luasnip.expand_or_jump()
-  else
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Tab>', true, true, true), 'n', true)
-  end
-end, { silent = true })
-
-vim.keymap.set({ 'i', 's' }, '<S-Tab>', function()
-  if luasnip.jumpable(-1) then luasnip.jump(-1) end
-end, { silent = true })
 
 -- Keybinds for moving line up or down with alt + j or k
 vim.keymap.set('n', '<A-j>', ':m .+1<CR>==', { silent = true })
