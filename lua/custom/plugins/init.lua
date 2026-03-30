60f7ca13a43bcae29ae2378199c2e2ce2d4cb44a
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-03-30 21:59:24 +0530

    feat: Added this for you: Ctrl + / now toggles comments across languages via Comment.nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -9,4 +9,25 @@ return {
   {
     'tpope/vim-fugitive',
   },
+  {
+    'numToStr/Comment.nvim',
+    opts = {},
+    config = function(_, opts)
+      require('Comment').setup(opts)
+
+      local api = require 'Comment.api'
+      local function toggle_visual_comment()
+        local esc = vim.api.nvim_replace_termcodes('<Esc>', true, false, true)
+        vim.api.nvim_feedkeys(esc, 'nx', false)
+        api.toggle.linewise(vim.fn.visualmode())
+      end
+
+      vim.keymap.set('n', '<C-_>', api.toggle.linewise.current, { desc = 'Toggle comment' })
+      vim.keymap.set('x', '<C-_>', toggle_visual_comment, { desc = 'Toggle comment selection' })
+
+      -- Some terminals send <C-/> and others send <C-_>, so map both.
+      vim.keymap.set('n', '<C-/>', api.toggle.linewise.current, { desc = 'Toggle comment' })
+      vim.keymap.set('x', '<C-/>', toggle_visual_comment, { desc = 'Toggle comment selection' })
+    end,
+  },
 }
