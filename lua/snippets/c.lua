c71dc1efee12b205fc90b0dd62946ea7bcec580e
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-04-16 10:21:10 +0530

    feat(snippets): add C competitive programming templates

---
+++ lua/snippets/c.lua
@@ -0,0 +1,116 @@
+local ls = require 'luasnip'
+
+local ps = ls.parser.parse_snippet
+
+return {
+  ps({ trig = 'cpbasic', dscr = 'Minimal C CP template with basic debugging' }, [[
+/*
+Author: Rahul Samant
+Created: $CURRENT_YEAR-$CURRENT_MONTH-$CURRENT_DATE $CURRENT_HOUR:$CURRENT_MINUTE:$CURRENT_SECOND
+*/
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef LOCAL
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+static void setup_io(void) {
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+static void solve(void) {
+    int n;
+    if (scanf("%d", &n) != 1) {
+        return;
+    }
+
+    int *arr = (int *)malloc((size_t)n * sizeof(int));
+    if (!arr) {
+        return;
+    }
+
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &arr[i]);
+    }
+
+    $0
+
+    free(arr);
+}
+
+int main(void) {
+    setup_io();
+
+    int t = 1;
+    if (scanf("%d", &t) != 1) {
+        return 0;
+    }
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+  ]]),
+
+  ps({ trig = 'cpa', dscr = 'Simple C CP template with single-case main' }, [[
+/*
+Author: Rahul Samant
+Created: $CURRENT_YEAR-$CURRENT_MONTH-$CURRENT_DATE $CURRENT_HOUR:$CURRENT_MINUTE:$CURRENT_SECOND
+*/
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#ifdef LOCAL
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+static void setup_io(void) {
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+int main(void) {
+    setup_io();
+
+    int n;
+    if (scanf("%d", &n) != 1) {
+        return 0;
+    }
+
+    int arr[n];
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &arr[i]);
+    }
+
+    $0
+
+    return 0;
+}
+  ]]),
+
+  ps({ trig = 'basicc', dscr = 'Bare-bones C starter' }, [[
+#include <stdio.h>
+
+int main(void) {
+    $0
+    return 0;
+}
+  ]]),
+}
