7e54c497d458877b947d3d2cb7842f1a828f98e3
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-04-18 20:36:54 +0530

    refactor: update autopairs plugin config

--- lua/kickstart/plugins/autopairs.lua
+++ lua/kickstart/plugins/autopairs.lua
@@ -6,5 +6,30 @@
 return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
-  opts = {},
+  opts = {
+    disable_filetype = { 'TelescopePrompt', 'spectre_panel', 'snacks_picker_input' },
+  },
+  config = function(_, opts)
+    local npairs = require 'nvim-autopairs'
+    local Rule = require 'nvim-autopairs.rule'
+
+    npairs.setup(opts)
+
+    -- Keep core pairing behavior consistent across these languages.
+    local c_like_filetypes = { 'c', 'cpp', 'rust', 'java' }
+    local c_like_pairs = {
+      { '(', ')' },
+      { '[', ']' },
+      { '{', '}' },
+      { '"', '"' },
+      { "'", "'" },
+    }
+
+    local rules = {}
+    for _, pair in ipairs(c_like_pairs) do
+      table.insert(rules, Rule(pair[1], pair[2], c_like_filetypes))
+    end
+
+    npairs.add_rules(rules)
+  end,
 }
