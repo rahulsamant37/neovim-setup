0bd3e4662b7c4e003cdaff5a9bc7e0be6e0915e2
Rahul Samant <rahulsamantcoc2@gmail.com>
2026-04-18 20:36:54 +0530

    refactor: update lint plugin config

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,6 +7,17 @@ return {
   event = { 'BufReadPre', 'BufNewFile' },
   config = function()
     local lint = require 'lint'
+    local lint_events = { 'BufEnter', 'BufWritePost', 'InsertLeave' }
+
+    local function try_lint_if_modifiable()
+      -- Only run the linter in buffers that you can modify in order to
+      -- avoid superfluous noise, notably within the handy LSP pop-ups that
+      -- describe the hovered symbol using Markdown.
+      if vim.bo.modifiable then
+        lint.try_lint()
+      end
+    end
+
     lint.linters_by_ft = {
       markdown = { 'markdownlint' },
     }
@@ -46,14 +57,9 @@ return {
     -- Create autocommand which carries out the actual linting
     -- on the specified events.
     local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+    vim.api.nvim_create_autocmd(lint_events, {
       group = lint_augroup,
-      callback = function()
-        -- Only run the linter in buffers that you can modify in order to
-        -- avoid superfluous noise, notably within the handy LSP pop-ups that
-        -- describe the hovered symbol using Markdown.
-        if vim.bo.modifiable then lint.try_lint() end
-      end,
+      callback = try_lint_if_modifiable,
     })
   end,
 }
